6225f87278c2bf6588fd01d09690e45f281e677e
Faisal Anwar <faisaldev7@gmail.com>
2024-09-01 19:33:48 +0530

    Support different delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,15 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers = numbers.gsub("\n", ",")
-    numbers.split(",").map(&:to_i).sum
+
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[4..]
+    else
+      delimiter = ","
+    end
+
+    numbers = numbers.gsub("\n", delimiter)
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
